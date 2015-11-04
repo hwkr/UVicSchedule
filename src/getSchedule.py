3aac35597e34326d68eda2b4aebfb9c9bd86ec23
Ben Hawker <ben@hawker.cc>
2015-11-03 20:03:01 -0800

    Parses the course titles

--- src/getSchedule.py
+++ src/getSchedule.py
@@ -1,11 +1,13 @@
 import sys
-import datetime
+import re
 import logging
 
 from bs4 import BeautifulSoup
 
 import uvic
 
+
+COURSE_TITLE_RE = re.compile(r"[^-]+- ([A-Z]+ [0-9]+) - [A-Z][0-9]+")
 
 def main():
     # Set up logging
@@ -20,19 +22,17 @@ def main():
 
     # Once we're done with the login (if necessary), the final response object url should be at MyCard history
     logging.debug('Parsing output')
-    timetable_html = response.read();
+    timetable_html = response.read()
     timetable_soup = BeautifulSoup(timetable_html, "html.parser")
 
     courses = timetable_soup.find_all('table', attrs={
         "class": "datadisplaytable",
         "summary": "This layout table is used to present the schedule course detail"
     })
-
-    # Save our newly found out data and the time to the file
-    logging.debug('Updating balance file')
-    outputString = "balance=" + ufsBalance + ";date=" + datetime.datetime.now().strftime("%a %b %d, %I:%M%p") + "\n"
-
-    print(outputString)
+    for course in courses:
+        title = course.find("caption").string
+        course_code = COURSE_TITLE_RE.match(title).groups()[0]
+        print course_code
 
     logging.info('Done')
 
